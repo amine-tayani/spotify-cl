561cac3d36ae50d4b410b6343e96f75e2130b73d
Amine Tayani <amine.tayanipro@gmail.com>
2023-05-14 20:30:25 +0100

    Added search resut Layout

--- src/app/search/@results/[keyword]/page.tsx
+++ src/app/search/@results/[keyword]/page.tsx
@@ -1,15 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function SearchResults({
   params,
 }: {
   params: { keyword: string };
 }) {
+  const types = [
+    "All",
+    "Podcast & Shows",
+    "Playlists",
+    "Profiles",
+    "Artists",
+    "Songs",
+    "Genres & Moods",
+    "Albums",
+  ];
+
   return (
-    <main>
-      <div className="text-white flex justify-center items-center ">
-        <span className="inline-flex items-center px-2 py-1 mr-2 text-sm font-medium text-gray-800 bg-gray-100 rounded">
-          {params.keyword}
-          <h1>hello</h1>
-        </span>
+    <main className="relative py-3 min-h-screen">
+      <div className="-ml-16">
+        <li className="flex items-center justify-center fixed z-50 w-full">
+          {types.map((type, id) => (
+            <Link
+              href={`/search/${encodeURIComponent(params.keyword)}/${type}`}
+              key={id}
+              className={`inline-flex items-center px-4 py-1.5 mr-2 text-sm ${
+                id === 0
+                  ? "bg-neutral-200 text-black hover:bg-white"
+                  : "text-neutral-50 hover:text-white bg-[--background-elevated-base] hover:bg-[--background-elevated-highlight]"
+              }  rounded-full`}
+            >
+              {type}
+            </Link>
+          ))}
+        </li>
+      </div>
+      <div className="grid sm:grid-cols-1 lg:grid-cols-2 mt-8 mx-72">
+        <div className=" p-8 md:p-12">
+          <h1 className=" text-white text-2xl font-extrabold mb-2">
+            Top result
+          </h1>
+          <div className="space-y-3 mt-3 p-6 bg-[--background-black] rounded-lg">
+            <Image
+              src="https://i.scdn.co/image/ab67616100005174fbe071f5bc42f38d3485a29a"
+              alt="Search image"
+              width={180}
+              height={180}
+              className="w-24 h-24 rounded-full"
+            />
+            <h2 className="text-3xl text-white font-bold">YOASOBI</h2>
+            <span className="inline-flex items-center px-3 py-1.5 text-sm font-semibold text-neutral-50 hover:text-white bg-[--background-base] rounded-full">
+              Artist
+            </span>
+          </div>
+        </div>
+        <div className=" p-8 md:p-12">
+          <h2 className=" text-white text-3xl font-extrabold mb-2">
+            Start with Flowbite Design System
+          </h2>
+          <p className="text-lg font-normal text-neutral-300 mb-4">
+            Static websites are now used to bootstrap lots of websites and are
+          </p>
+        </div>
       </div>
     </main>
   );
--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 export default function SearchLayout({
   content,
@@ -12,27 +13,38 @@ export default function SearchLayout({
   results: React.ReactNode;
   children: React.ReactNode;
 }) {
-  const [search, setSearch] = React.useState("");
+  const [keyword, setKeyword] = React.useState("");
+
+  const router = useRouter();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
+    const value = e.target.value.trim();
+    setKeyword(value);
   };
+
+  React.useEffect(() => {
+    if (keyword) {
+      router.push(`/search/${encodeURIComponent(keyword)}`);
+    } else {
+      router.push(`/search`);
+    }
+  }, [keyword]);
 
   return (
     <div>
-      <div className="absolute z-50 top-0 left-0 right-0 mx-auto mt-6 w-[400px]">
+      <div className="absolute z-50 top-0 left-96 mx-6 mt-5 w-[400px]">
         <div className="absolute inset-y-0 left-0 flex items-center pl-4">
           <MagnifyingGlassIcon className="h-6 w-6 text-neutral-300" />
         </div>
         <input
           onChange={onChange}
-          value={search}
+          value={keyword}
           type="search"
-          className="bg-neutral-800 border-none text-neutral-300 placeholder:text-neutral-500 text-sm outline-none rounded-full block w-full pl-12 p-3 focus:ring-2 focus:ring-neutral-200"
+          className="bg-[--background-elevated-base] hover:bg-[--background-elevated-highlight] hover:ring-1 hover:ring-neutral-600 border-none text-neutral-300 placeholder:text-neutral-500 text-sm outline-none rounded-full block w-full pl-12 p-3.5 focus:ring-2 focus:ring-neutral-200"
           placeholder="What do you want to listen to?"
         />
       </div>
-      {search ? results : content}
+      {keyword ? results : content}
       <div>{children}</div>
     </div>
   );
