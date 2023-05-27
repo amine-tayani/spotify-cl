2651b1eaf8c3edff48d3ff3f9d4a9ff8c54c2f91
Amine Tayani <amine.tayanipro@gmail.com>
2023-05-27 14:31:41 +0100

    finish signup page layout

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -12,28 +12,7 @@ export default function SignupPage() {
       <h1 className="text-xl text-center font-bold text-white my-6">
         Sign up for free to start listening.
       </h1>
-      <div className="mb-6">
-        <button
-          className="rounded-full text-neutral-100 font-semibold bg-[--background-base] text-center inline-flex justify-center border border-neutral-500 relative select-none"
-          style={{
-            paddingInline: "40px",
-            paddingBlock: "12px",
-            verticalAlign: "middle",
-            inlineSize: "100%",
-          }}
-        >
-          <span
-            style={{
-              background:
-                "url(https://accounts.scdn.co/sso/images/new-google-icon.72fd940a229bc94cf9484a3320b3dccb.svg) no-repeat center center",
-            }}
-            className="w-6 h-6"
-          ></span>
-          <span className="capitalize m-auto flex flex-row gap-x-3 items-center">
-            Continue with Google
-          </span>
-        </button>
-      </div>
+
       <div className="mb-6">
         <label
           htmlFor="email"
@@ -45,7 +24,7 @@ export default function SignupPage() {
           type="email"
           id="email"
           className=" bg-[--background-base] border placeholder:text-neutral-400 border-neutral-500 text-neutral-100 text-sm rounded-sm block w-full p-3"
-          placeholder="Email or username"
+          placeholder="Type your email"
           required
         />
       </div>
@@ -84,7 +63,7 @@ export default function SignupPage() {
         type="submit"
         className="bg-[--text-bright-accent] focus:outline-none font-semibold rounded-full px-5 py-2.5 text-center w-full"
       >
-        Log In
+        Sign up
       </button>
     </form>
   );
