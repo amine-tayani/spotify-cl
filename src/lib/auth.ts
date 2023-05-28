ca1c2d3847bc16883ad4994b1a66b3a294b18b14
Amine Tayani <amine.tayanipro@gmail.com>
2023-05-28 11:23:27 +0100

    refactor: add callbacks to next auth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,30 @@
-import { NextAuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID!,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: process.env.SPOTIFY_API_SCOPE!,
+        },
+      },
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id = account.providerAccountId;
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.userId = token.id;
+      session.user.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
